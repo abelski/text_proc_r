90913783b3c392bb38d8b3ed5fd7198cdb4654a6
Artur_Belski <artur_belski@epam.com>
2019-02-22 20:29:29 +0300

    initial

--- main.r
+++ main.r
@@ -12,20 +12,37 @@ cleanFun <- function(htmlString) {
 
 
 m <-  mongo("tickets", url = "mongodb://localhost:27017/agile")
-m <- m$find("{}","{}",limit=10)$description
+m <- m$find("{}","{}")$description
+# m <- m$find("{}","{}",limit=100)$description
 m <- cleanFun(paste(m,collapse=" "))
 
 docs <- Corpus(VectorSource(m))
 
+
 toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+
 docs <- tm_map(docs, toSpace, "/")
 docs <- tm_map(docs, toSpace, "@")
 docs <- tm_map(docs, toSpace, "\\|")
 docs <- tm_map(docs, content_transformer(tolower))
 docs <- tm_map(docs, removeNumbers)
 docs <- tm_map(docs, removeWords, stopwords("english"))
-docs <- tm_map(docs, removeWords, c("wwwepamcom", "epamcomgt","dear","epamcomgt","epamcom"))
 docs <- tm_map(docs, removePunctuation)
+
+junkList <- c(
+"agile",
+"sd",
+"dear",
+"epamcom",
+"epamcomgt",
+"https",
+"please",
+"subject",
+"wwwepamcom",
+"http"
+
+)
+docs <- tm_map(docs, removeWords, junkList)
 docs <- tm_map(docs, stripWhitespace)
 
 # inspect(docs)
@@ -35,10 +52,10 @@ m <- as.matrix(dtm)
 v <- sort(rowSums(m),decreasing=TRUE)
 d <- data.frame(word = names(v),freq=v)
 
-d <- head(d, 20)
+d <- head(d, 100)
 
-print(d)
-
+# print(d)
+#
 set.seed(1234)
 wordcloud(words = d$word, freq = d$freq, min.freq = 1,
 max.words=200, random.order=FALSE, rot.per=0.35,
